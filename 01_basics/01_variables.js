cf3705ca849f9ec055407bbc38e56b8830200f95
YVVERMA <yv843959@gmail.com>
2024-08-05 19:57:35 +0000

    discussion on variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,32 @@
+const userName = "Hello Wourld!"
+let userId = "4564798";
+userId  = "javaScript";
+// var userCity = "dewas";
+// userCity = "indore"; 
+
+var userCity = "MP";
+
+
+
+console.table([userName,userId,userCity])
+
+// {
+//   this is a  scope
+// }
+
+/*
+Prefer not to use var because of issue is block scope and functional scope
+*/
+
+
+
+
+// const userName = 
+
+
+
+// console.log(userId);
+
+
+// console.log(userName);
+
