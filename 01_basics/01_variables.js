f463ebeede26425a55e408de6f9dee49db0b1e0b
YVVERMA <yv843959@gmail.com>
2024-08-05 19:31:50 +0000

    discussion on variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,32 @@
+const accountID = 324856;
+let accountEmail = "yv@google.com";
+var accountPassword = "2341657";
+
+
+let userName;
+accountEmail = "yash.com";
+accountPassword = 5435421;
+
+console.table([userName,accountEmail,accountPassword])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+ 
+//  {
+
+//  }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
