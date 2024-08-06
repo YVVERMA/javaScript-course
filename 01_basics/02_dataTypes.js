68bbe8a3863cc393389e14d5e69372e856e228b5
Yash Verma <yv843959@gmail.com>
2024-08-06 09:52:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+
+
+// alert(456 + 123);
+
+let name = "yash";
+let age = 18;
+let isLoggedIn = true;
+let state = null;
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean 
+// null
+// undefined
+
+//  object 
+
+console.log(typeof name);
+console.log(typeof age );
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null);
+
+
+
+
+
+
