f9e1e231bf38f827dc87b505533dcafab7ea1345
Yash Verma <yv843959@gmail.com>
2024-08-07 08:06:56 +0000

    Conversion confusion

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,19 @@
+let score = null;
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+// console.log(typeof(score));
+
+console.log(valueInNumber);
+
+// "123" => 123
+// "123asc" => NaN
+// true => 1; false => 0;
+
+
+
+
+
+
+
